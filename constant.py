ee7b451ad876e6495ef424a4bad5300281ac9c0d
Siobhan Jacobson <siobhan.jacobson@gmail.com>
2020-04-01 20:58:35 -0400

    Start work of reading from CSV files.

---
+++ constant.py
@@ -0,0 +1,16 @@
+SITE_FIELD_MAP = {
+    "name": "site name",
+    "code": "site code",
+    "state": "state",
+    "system": "system",
+    "latitude": "latitude",
+    "longitude": "longitude",
+}
+
+SAMPLE_FIELD_MAP = {
+    "month": "sample month",
+    "day": "sample day",
+    "year": "sample day",
+    "type": "sample type",
+    "depth": "sample depth (cm)"
+}
--- test_import.py
+++ test_import.py
@@ -1,28 +1,35 @@
+import constant
 from pymongo import MongoClient
+from csv import DictReader
 
-client = MongoClient()
+with open('chen_import/sediment_individual.csv', newline='') as csv_file:
+    reader = DictReader(csv_file)
+    for row in reader:
+        print(row['site name'], row['site code'])
 
-db = client.euc_test
-
-site = {"name": "Above",
-        "code": "A",
-        "state": "ME",
-        "system": "Penobscot River",
-        "latitude": 44.76309,
-        "longitude": -68.80048}
-
-sites = db.sites
-site_id = sites.insert_one(site).inserted_id
-sample = {"month": "July",
-          "year": 2016,
-          "type": "grab",
-          "depth": "0-2",
-          "kind": "sediment",
-          "molecule": "methylmercury",
-          "units": "ng/g DW",
-          "value": 1.62,
-          "sited_id": site_id,
-          "chebi_id": 30785}
-
-samples = db.samples
-samples.insert_one(sample)
+# client = MongoClient()
+#
+# db = client.euc_test
+#
+# site = {"name": "Above",
+#         "code": "A",
+#         "state": "ME",
+#         "system": "Penobscot River",
+#         "latitude": 44.76309,
+#         "longitude": -68.80048}
+#
+# sites = db.sites
+# site_id = sites.insert_one(site).inserted_id
+# sample = {"month": "July",
+#           "year": 2016,
+#           "type": "grab",
+#           "depth": "0-2",
+#           "kind": "sediment",
+#           "molecule": "methylmercury",
+#           "units": "ng/g DW",
+#           "value": 1.62,
+#           "sited_id": site_id,
+#           "chebi_id": 30785}
+#
+# samples = db.samples
+# samples.insert_one(sample)
