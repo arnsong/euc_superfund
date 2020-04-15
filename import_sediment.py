9a84eede36cc756ba25d8719924a137a8ee4949a
Siobhan Jacobson <siobhan.jacobson@gmail.com>
2020-04-15 15:54:54 -0400

    EUC-1 Insert documents into a Mongo database.

--- import_sediment.py
+++ import_sediment.py
@@ -18,37 +18,14 @@ def assign_values(doc, row):
                 doc[k] = v
 
 
+client = MongoClient()
+db = client.euc_prototype
+
+examples = db.examples
+
 with open('chen_import/sediment_individual.csv', newline='') as csv_file:
     reader = DictReader(csv_file)
     for current_row in reader:
         document = deepcopy(constant.SEDIMENT_FIELD_MAP)
         assign_values(document, current_row)
-        print("Filled in doc: {0}".format(document))
-
-
-# client = MongoClient()
-#
-# db = client.euc_test
-#
-# site = {"name": "Above",
-#         "code": "A",
-#         "state": "ME",
-#         "system": "Penobscot River",
-#         "latitude": 44.76309,
-#         "longitude": -68.80048}
-#
-# sites = db.sites
-# site_id = sites.insert_one(site).inserted_id
-# sample = {"month": "July",
-#           "year": 2016,
-#           "type": "grab",
-#           "depth": "0-2",
-#           "kind": "sediment",
-#           "molecule": "methylmercury",
-#           "units": "ng/g DW",
-#           "value": 1.62,
-#           "sited_id": site_id,
-#           "chebi_id": 30785}
-#
-# samples = db.samples
-# samples.insert_one(sample)
+        examples.insert_one(document)
