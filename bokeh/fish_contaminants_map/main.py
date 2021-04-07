ae844ad58cdb1385583fbe5e7958378d4bb7e260
Arnold Song <Arnold.J.Song@dartmouth.edu>
2021-04-07 12:39:15 -0400

    Refactored fish_contaminants_map app for readability

--- bokeh/fish_contaminants_map/main.py
+++ bokeh/fish_contaminants_map/main.py
@@ -2,119 +2,111 @@ from bokeh.models import (ColumnDataSource,
                           GMapOptions, 
                           HoverTool,
                           ColorBar,
-                          MultiSelect,
-                          LogColorMapper, 
-                          LogTicker)
+                          MultiSelect)
 from bokeh.models.formatters import PrintfTickFormatter
 from bokeh.plotting import gmap, curdoc
-from bokeh.transform import linear_cmap
 from bokeh.layouts import column, row
-import colorcet as cc
 
 import pandas as pd
+import config
 
-# Import data from xlsx spreadsheet
-data = pd.read_excel('/root/app/fish_contaminants_map/Lake_fish_tissue_study_location_5Mar2021.xlsx')
+# Visualization components
+class Map:
 
-analytes = [ "Mercury ng/g (ppb)", \
-            "Total dichloro biphenyls ng/g (ppb)", \
-            "Total heptachloro biphenyls ng/g (ppb)", \
-            "Total hexachloro biphenyls ng/g (ppb)", \
-            "Total monochloro biphenyls ng/g (ppb)", \
-            "Total nonachloro biphenyls ng/g (ppb)", \
-            "Total octachloro biphenyls ng/g (ppb)", \
-            "Total PCBs ng/g (ppb)", \
-            "Total pentachloro biphenyls ng/g (ppb)", \
-            "Total tetrachloro biphenyls ng/g (ppb)", \
-            "Total trichloro biphenyls ng/g (ppb)" ]
+    def __init__(self): 
 
-# Initialize Bokeh document, which will contain all of the models
-doc = curdoc()
+        # Initialize bokeh document
+        self.doc = curdoc()
 
-# Initialize multiselect widget that will allow the user to select the 
-# analyte of interest for the map
-multiselect = MultiSelect(value=["Mercury ng/g (ppb)"], options=analytes, width=400, height=500)
+        # Load data
+        self.data = pd.read_excel(config.DATAFILE)
+        self.analytes = self.data.columns[11:-2].tolist()
 
-# Initialize Google map basemap
-gmap_key = "AIzaSyDfvhnKkOAhVDaM0Y4tcASqSJ1oJFUARW8"
-analyte_map = gmap(gmap_key, 
-                   GMapOptions(lat=40.2861, lng=-97.7394, map_type="roadmap", zoom=4), 
-                   frame_width=800, frame_height=500)
+        # Filter data
+        self.map_data = self.filter_data(self.analytes[0], self.data)
 
-# Filter fish data
-analyte = multiselect.value[0]
-map_data = data.loc[:, [ analyte, 'Site_Name', 'Latitude', 'Longitude' ]].replace(' ', 'NaN')
-map_data[analyte] = map_data[analyte].astype('float')
-map_data = map_data.dropna()
-map_data['analyte'] = multiselect.value[0]
+        # Initialize multiselect widget that will allow the user to select the 
+        # analyte of interest for the map
+        self.multiselect = MultiSelect(value=[self.analytes[0]], options=self.analytes, width=400, height=500)
+        self.multiselect.on_change('value', lambda attr, old, new: self.update())
 
-# Remove rows with missing values
-map_data = map_data.dropna()
+        # Google Maps basemap
+        self.map = gmap(config.GMAPS_API_KEY, 
+                        GMapOptions(lat=40.2861, lng=-97.7394, map_type="roadmap", zoom=4), 
+                        frame_width=800, frame_height=500)
 
-source = ColumnDataSource(
-    data = dict(analyte=map_data['analyte'],
-                result=map_data[multiselect.value[0]],
-                lat=map_data['Latitude'],
-                lon=map_data['Longitude'],
-                site_id=map_data['Site_Name'])
-)
+        # Hover tool
+        self.hovertool = self.map.add_tools(
+                            HoverTool(tooltips=[("Site ID", "@site_id"),
+                                                ("Analyte", "@analyte"),
+                                                ("Result", "@result")],
+                                      mode="mouse", point_policy="follow_mouse"))
 
-# Add a color bar for scale to the map
-color_mapper = LogColorMapper(palette=cc.CET_L17, 
-            low=min(map_data[multiselect.value[0]]), 
-            high=max(map_data[multiselect.value[0]]))
+        # Color mapper
+        self.color_mapper = config.COLORMAPPER(palette=config.COLORMAP, 
+                                               low=min(self.map_data[self.analytes[0]]), 
+                                               high=max(self.map_data[self.analytes[0]]))
+        
+        # Data points
+        self.source = ColumnDataSource(
+            data = dict(analyte=self.map_data['analyte'],
+                        result=self.map_data[self.analytes[0]],
+                        lat=self.map_data['Latitude'],
+                        lon=self.map_data['Longitude'],
+                        site_id=self.map_data['Site_Name']))
 
-circles = analyte_map.circle(x="lon", y="lat", size=10, line_color='white', 
-                            fill_color={'field': 'result', 'transform': color_mapper},
-                            fill_alpha=0.8, source=source)
+        self.datapts = self.map.circle(x="lon", y="lat", 
+                                       size=10, line_color='white', 
+                                       fill_color={'field': 'result', 
+                                                   'transform': self.color_mapper},
+                                       fill_alpha=0.8, 
+                                       source=self.source)
 
-color_bar = ColorBar(color_mapper=color_mapper, 
-        label_standoff=10, 
-        formatter=PrintfTickFormatter(format='%4.1e'),
-        ticker=LogTicker(desired_num_ticks=4), 
-        border_line_color=None, 
-        orientation='vertical', 
-        location=(40,0))
-analyte_map.add_layout(color_bar, 'left')
+        # Color bar 
+        self.color_bar = ColorBar(color_mapper=self.color_mapper, 
+                                  label_standoff=10, 
+                                  formatter=PrintfTickFormatter(format='%4.1e'),
+                                  ticker=config.TICKER(desired_num_ticks=4), 
+                                  border_line_color=None, 
+                                  orientation='vertical', 
+                                  location=(40,0))
+        
+        # Layout visualization
+        # Add color bar to map rather than document.  It will be easier to control
+        self.map.add_layout(self.color_bar, 'left')
+        self.doc.add_root(row(column(self.multiselect, width=400), self.map))
 
-# Add feature that displays the site information on mouse over
-analyte_map.add_tools(HoverTool(
-        tooltips=[("Site ID", "@site_id"),
-                  ("Analyte", "@analyte"),
-                  ("Result", "@result")],
-        mode="mouse", point_policy="follow_mouse"
-))
+    @staticmethod
+    def filter_data(analyte, data):
+        filtered_data = data.loc[:, [ analyte, 'Site_Name', 'Latitude', 'Longitude' ]].replace(' ', 'NaN')
+        filtered_data[analyte] = filtered_data[analyte].astype('float')
+        filtered_data = filtered_data.dropna()
+        filtered_data['analyte'] = analyte
 
-# Layout document
-doc.add_root(row(column(multiselect, width=400), analyte_map))
+        return filtered_data
 
-# Function to update map based on selected analyte
-def update():
-    
-    analyte = multiselect.value[0]
-    df = data.loc[:, [analyte, 'Site_Name', 'Latitude', 'Longitude' ]].replace(' ', 'NaN')
-    df[analyte] = df[analyte].astype('float')
-    df = df.dropna()
-    
-    if df is not None:
+    def update(self):
 
-        color_mapper.low = min(df[analyte])
-        color_mapper.high = max(df[analyte])
+        analyte = self.multiselect.value[0]
+        map_data = self.filter_data(analyte, self.data)
+        
+        if map_data is not None:
 
-        df['analyte'] = analyte
+            map_data['analyte'] = analyte
 
-        source.data = dict(analyte=df['analyte'],
-                           result=df[analyte],
-                           lat=df['Latitude'],
-                           lon=df['Longitude'],
-                           site_id=df['Site_Name'])
-    else:
-        source.data = dict(analyte=[],
-                           result=[],
-                           lat=[],
-                           lon=[],
-                           site_id=[])
+            self.color_mapper.low = min(map_data[analyte])
+            self.color_mapper.high = max(map_data[analyte])
+            self.source.data = dict(analyte=map_data['analyte'],
+                                    result=map_data[analyte],
+                                    lat=map_data['Latitude'],
+                                    lon=map_data['Longitude'],
+                                    site_id=map_data['Site_Name'])
+        else:
+            self.source.data = dict(analyte=[],
+                                    result=[],
+                                    lat=[],
+                                    lon=[],
+                                    site_id=[])
 
-multiselect.on_change('value', lambda attr, old, new: update())
 
-update()
+map = Map()
---
+++ fish_contaminants_import/config.py
@@ -0,0 +1,4 @@
+DB_USER = 'postgres'
+DB_PASSWORD = 'llihkao5'
+DB_HOST = 'toxic-metals.dartmouth.edu:5433'
+DB_NAME = 'fish_contaminants'
